2b63aac07ad6c6f07b9d6009a3bace55a98cd1ed
depetrol <lslrobert@outlook.com>
2024-08-28 03:02:46 +0000

    remove unused code

--- python/sglang/srt/models/openvla.py
+++ python/sglang/srt/models/openvla.py
@@ -48,22 +48,6 @@ logger = logging.getLogger(__name__)
 IGNORE_INDEX = -100
 
 
-# === Main HF Class Definitions ===
-@dataclass
-class PrismaticCausalLMOutputWithPast(ModelOutput):
-    """Base class for Prismatic casual (visually-conditioned) language model outputs; also exposes visual features."""
-
-    loss: Optional[torch.FloatTensor] = None
-    logits: torch.FloatTensor = None
-    next_token_logits: torch.FloatTensor = None
-    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
-    hidden_states: Optional[Tuple[torch.FloatTensor, ...]] = None
-    attentions: Optional[Tuple[torch.FloatTensor]] = None
-
-    # Additions for VLMs
-    projector_features: Optional[torch.FloatTensor] = None
-
-
 class OpenVLAConfig(PretrainedConfig):
     model_type: str = "openvla"
     is_composition: bool = False
@@ -158,15 +142,6 @@ class OpenVLAForActionPrediction(PreTrainedModel):
                 "TIMM Version must be >= 0.9.10 and < 1.0.0 (breaking); please raise a GitHub Issue "
                 "if you urgently need support for latest TIMM versions."
             )
-        if (transformers.__version__ != "4.40.1") or (
-            tokenizers.__version__ != "0.19.1"
-        ):
-            logger.warning(
-                f"Expected `transformers==4.40.1` and `tokenizers==0.19.1` but got "
-                f"`transformers=={transformers.__version__}` and `tokenizers=={tokenizers.__version__}`; "
-                f"there might be inference-time regressions due to dependency changes. If in doubt, please"
-                f"use the above versions."
-            )
 
         # Instantiate PrismaticVisionBackbone (w/ Potential Fused Backbone)
         self.vision_backbone = PrismaticVisionBackbone(
@@ -187,11 +162,6 @@ class OpenVLAForActionPrediction(PreTrainedModel):
         self.language_model = LlamaForCausalLM(
             config.text_config, quant_config=quant_config
         )
-        # self.language_model = AutoModelForCausalLM.from_config(
-        #     config.text_config, attn_implementation=config._attn_implementation
-        # )
-
-        # self.processor = PrismaticProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
 
         self.vocab_size = config.text_config.vocab_size
         self.pad_token_id = config.pad_token_id
@@ -242,28 +212,6 @@ class OpenVLAForActionPrediction(PreTrainedModel):
 
         self.language_model.load_weights(weights)
 
-    def _init_weights(self, module: nn.Module) -> None:
-        # Important :: this HF ported version is *not* meant for training from scratch; only inference and fine-tuning!
-        #   => As such, this init_weights code is not correct; if training VLMs from scratch, use the main codebase at
-        #      https://github.com/TRI-ML/prismatic-vlms
-        std = (
-            self.config.initializer_range
-            if hasattr(self.config, "initializer_range")
-            else self.config.text_config.initializer_range
-        )
-
-        if hasattr(module, "class_embedding"):
-            module.class_embedding.data.normal_(mean=0.0, std=std)
-
-        if isinstance(module, (nn.Linear, nn.Conv2d)):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-
     def forward(
         self,
         input_ids: torch.LongTensor,
@@ -272,9 +220,7 @@ class OpenVLAForActionPrediction(PreTrainedModel):
         pixel_values: Optional[List[Optional[np.array]]] = None,
         image_sizes: Optional[List[List[int]]] = None,
         image_offsets: Optional[List[int]] = None,
-    ) -> Union[Tuple, PrismaticCausalLMOutputWithPast]:
-        """Run a forward pass through the VLM, returning a PrismaticCausalLMOutputWithPast instance."""
-        # Instantiate Placeholder for Projector Features
+    ):
         projected_patch_embeddings = None
 
         need_vision = pixel_values is not None and any(
@@ -314,103 +260,6 @@ class OpenVLAForActionPrediction(PreTrainedModel):
             input_metadata=input_metadata,
             input_embeds=input_embeddings,
         )
-
-    def predict_action(
-        self,
-        input_ids: Optional[torch.LongTensor] = None,
-        unnorm_key: Optional[str] = None,
-        **kwargs: str,
-    ) -> np.ndarray:
-        """Thin wrapper around super().generate() that decodes predicted actions and de-normalizes them."""
-
-        # If the special empty token ('') does not already appear after the colon (':') token in the prompt
-        # (after "OUT:" or "ASSISTANT:"), insert it to match the inputs seen at training time
-        if not torch.all(input_ids[:, -1] == 29871):
-            input_ids = torch.cat(
-                (
-                    input_ids,
-                    torch.unsqueeze(torch.Tensor([29871]).long(), dim=0).to(
-                        input_ids.device
-                    ),
-                ),
-                dim=1,
-            )
-
-        # Run VLA inference
-        generated_ids = self.generate(
-            input_ids, max_new_tokens=self.get_action_dim(unnorm_key), **kwargs
-        )
-        # generated_ids = self.generate(input_ids, max_new_tokens=7, **kwargs)
-
-        # Run VLA inference without KV-Cache
-        # generated_ids = input_ids.clone()
-        # self.past_key_values = None
-        # for _ in range(7):
-        #     output = self.generate(generated_ids, max_new_tokens=1, **kwargs)
-        #     generated_ids = torch.cat((generated_ids, output[:, -1:]), dim=-1)
-
-        # Extract predicted action tokens and translate into (normalized) continuous actions
-        predicted_action_token_ids = (
-            generated_ids[0, -self.get_action_dim(unnorm_key) :].cpu().numpy()
-        )
-        discretized_actions = self.vocab_size - predicted_action_token_ids
-        discretized_actions = np.clip(
-            discretized_actions - 1, a_min=0, a_max=self.bin_centers.shape[0] - 1
-        )
-        normalized_actions = self.bin_centers[discretized_actions]
-
-        # Unnormalize actions
-        action_norm_stats = self.get_action_stats(unnorm_key)
-        mask = action_norm_stats.get(
-            "mask", np.ones_like(action_norm_stats["q01"], dtype=bool)
-        )
-        action_high, action_low = np.array(action_norm_stats["q99"]), np.array(
-            action_norm_stats["q01"]
-        )
-        actions = np.where(
-            mask,
-            0.5 * (normalized_actions + 1) * (action_high - action_low) + action_low,
-            normalized_actions,
-        )
-
-        return actions
-
-    @staticmethod
-    def _check_unnorm_key(
-        norm_stats: Dict[str, Dict[str, Any]], unnorm_key: Optional[str]
-    ) -> str:
-        if unnorm_key is None and len(norm_stats) != 1:
-            raise ValueError(
-                f"Your model was trained on more than one dataset. "
-                f"Please pass a `unnorm_key` from the following options to choose the statistics used for "
-                f"de-normalizing actions: {norm_stats.keys()}"
-            )
-
-        # If None, grab the (singular) dataset in `norm_stats` to use as `unnorm_key`
-        unnorm_key = (
-            unnorm_key if unnorm_key is not None else next(iter(norm_stats.keys()))
-        )
-        if unnorm_key not in norm_stats:
-            raise ValueError(
-                f"The `unnorm_key` you chose ({unnorm_key = }) is not in the available statistics. "
-                f"Please choose from: {norm_stats.keys()}"
-            )
-
-        return unnorm_key
-
-    def get_action_dim(self, unnorm_key: Optional[str] = None) -> int:
-        """Get the dimensionality of the policy's action space."""
-        unnorm_key = self._check_unnorm_key(self.norm_stats, unnorm_key)
-        return len(self.norm_stats[unnorm_key]["action"]["q01"])
-
-    def get_action_stats(self, unnorm_key: Optional[str] = None) -> Dict[str, Any]:
-        """Get all the logged statistics for the given dataset."""
-        unnorm_key = self._check_unnorm_key(self.norm_stats, unnorm_key)
-        return self.norm_stats[unnorm_key]["action"]
-
-    def tie_weights(self) -> None:
-        return
-        # self.language_model.tie_weights()  # Note: `Llama-2` and `Mistral` don't tie weights (no-op)
 
 
 EntryClass = OpenVLAForActionPrediction
